ae6d70e1b406c548329edbba1d452cb18af32f76
Mathias <mrboen94@gmail.com>
2021-09-02 19:41:12 +0200

    need to modify index.tsx before it can be committed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,23 @@
+const { colors } = require("tailwindcss/defaultTheme");
+
 module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
 
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        teal: colors.teal,
+        cyan: colors.cyan,
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
 };
