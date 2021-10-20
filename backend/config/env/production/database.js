956af9125c286f0c78e24bab3a8363a3e7375648
Mathias <mrboen94@gmail.com>
2021-10-20 14:36:06 +0200

    digitalocean implementation

--- backend/config/env/production/database.js
+++ backend/config/env/production/database.js
@@ -1,19 +1,23 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
+
+module.exports = () => ({
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', ''),
-        password: env('DATABASE_PASSWORD', ''),
+        client: "postgres",
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false,
+        },
       },
-      options: {
-        ssl: false,
-      },
+      options: {},
     },
   },
 });
