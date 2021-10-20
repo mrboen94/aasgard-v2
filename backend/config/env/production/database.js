0b71a5852351c54083b6704daf221f9acbebb9e5
Mathias <mrboen94@gmail.com>
2021-10-20 16:25:40 +0200

    test

--- backend/config/env/production/database.js
+++ backend/config/env/production/database.js
@@ -1,5 +1,5 @@
 const parse = require("pg-connection-string").parse;
-const config = parse(process.env.DATABASE_URL);
+const config = parse(process.env.DB_URL);
 
 module.exports = () => ({
   defaultConnection: "default",
