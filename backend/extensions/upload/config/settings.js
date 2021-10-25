976050600429a5ca94f1dbc2a17c94028aa6469c
Mathias Bøe <mathias@islandgarden.no>
2021-10-25 10:35:13 +0200

    revert the ooopsie

--- backend/extensions/upload/config/settings.js
+++ backend/extensions/upload/config/settings.js
@@ -4,6 +4,6 @@ module.exports = {
     key: process.env.DO_SPACE_ACCESS_KEY,
     secret: process.env.DO_SPACE_SECRET_KEY,
     endpoint: process.env.DO_SPACE_ENDPOINT,
-    directory: process.env.DO_SPACE_DIRECTORY,
+    space: process.env.DO_SPACE_BUCKET,
   },
 };
