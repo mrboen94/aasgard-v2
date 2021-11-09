f89b17c25a65a444b0cff89330956a6ab50b14d6
Mathias <mrboen94@gmail.com>
2021-11-09 15:23:24 +0100

    fixing image quote to work on all devices

--- frontend/components/contentBlocks/imageQuote.tsx
+++ frontend/components/contentBlocks/imageQuote.tsx
@@ -22,8 +22,12 @@ export default function ImageQuote({
             className="absolute inset-x-0 top-0 h-1/2 bg-white lg:hidden"
           />
           <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:p-0 lg:h-full">
-            <div className="aspect-w-10 aspect-h-6 rounded-xl shadow-xl overflow-hidden sm:aspect-w-16 sm:aspect-h-7 lg:aspect-none lg:h-full relative">
-              <Image fill imageStyle="rounded-xl" image={image} unoptimized />
+            <div className="aspect-w-10 aspect-h-6 rounded-xl shadow-xl overflow-hidden sm:aspect-w-16 sm:aspect-h-7 lg:aspect-none lg:h-full">
+              <img
+                className="object-cover lg:h-full lg:w-full"
+                src={image.url}
+                alt="image quote"
+              />
             </div>
           </div>
         </div>
