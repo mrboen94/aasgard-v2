217fcc88653dcc0c3bbd5bf20aa07cbbd03c6b91
Mathias Bøe <mrboen94@gmail.com>
2023-01-11 18:17:00 +0100

    retrying with new action in input field

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -39,7 +39,6 @@ function Newsletter() {
     <form
       name="newsletter"
       action="/thank-you"
-      method="POST"
       className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40"
       data-netlify="true"
     >
@@ -53,7 +52,6 @@ function Newsletter() {
       <div className="mt-6 flex">
         <input
           type="email"
-          name="email"
           placeholder="Email address"
           aria-label="Email address"
           required
