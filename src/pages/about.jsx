3b98d5ff261cf8a823a9f065f75f29627ae7b745
Mathias Bøe <mrboen94@gmail.com>
2023-01-14 17:03:13 +0100

    moving text to sanity

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,6 +2,11 @@ import Image from 'next/future/image'
 import Head from 'next/head'
 import Link from 'next/link'
 import clsx from 'clsx'
+import client from 'client'
+import groq from 'groq'
+import { serialize } from 'next-mdx-remote/serialize'
+import remarkGfm from 'remark-gfm'
+import rehypePrism from '@mapbox/rehype-prism'
 
 import { Container } from '@/components/Container'
 import {
@@ -11,6 +16,7 @@ import {
   LinkedInIcon,
 } from '@/components/SocialIcons'
 import portraitImage from '@/images/portrait.jpg'
+import { MDXRemote } from 'next-mdx-remote'
 
 function SocialLink({ className, href, children, icon: Icon }) {
   return (
@@ -37,7 +43,7 @@ function MailIcon(props) {
   )
 }
 
-export default function About() {
+export default function About({ about, bio }) {
   return (
     <>
       <Head>
@@ -61,36 +67,10 @@ export default function About() {
           </div>
           <div className="lg:order-first lg:row-span-2">
             <h1 className="font-display text-4xl tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-              I’m Mathias Bøe. I live in Bergen, where I help create a better
-              future with Bouvet.
+              {about.bioTitle}
             </h1>
-            <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
-              <p>
-                I’ve loved making things for as long as I can remember, and
-                wrote my first program when I was 6 years old, just two weeks
-                after my mom brought home the brand new Macintosh LC 550 that I
-                taught myself to type on.
-              </p>
-              <p>
-                The only thing I loved more than computers as a kid was space.
-                When I was 8, I climbed the 40-foot oak tree at the back of our
-                yard while wearing my older sister’s motorcycle helmet, counted
-                down from three, and jumped — hoping the tree was tall enough
-                that with just a bit of momentum I’d be able to get to orbit.
-              </p>
-              <p>
-                I spent the next few summers indoors working on a rocket design,
-                while I recovered from the multiple surgeries it took to fix my
-                badly broken legs. It took nine iterations, but when I was 15 I
-                sent my dad’s Blackberry into orbit and was able to transmit a
-                photo back down to our family computer from space.
-              </p>
-              <p>
-                Today, I’m the founder of Planetaria, where we’re working on
-                civilian space suits and manned shuttle kits you can assemble at
-                home so that the next generation of kids really <em>can</em>{' '}
-                make it to orbit — from the comfort of their own backyards.
-              </p>
+            <div className="prose mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
+              <MDXRemote {...bio} lazy />
             </div>
           </div>
           <div className="lg:pl-20">
@@ -123,11 +103,11 @@ export default function About() {
                 Follow on LinkedIn
               </SocialLink>
               <SocialLink
-                href="mailto:mrboen94@gmail.com"
+                href="mailto:mathias@stryn.dev"
                 icon={MailIcon}
                 className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
               >
-                mrboen94@gmail.com
+                mathias@stryn.dev
               </SocialLink>
             </ul>
           </div>
@@ -135,4 +115,32 @@ export default function About() {
       </Container>
     </>
   )
+}
+
+export async function getStaticProps() {
+  const about = await client.fetch(groq`*[_type == "staticInfo"]{
+    "images":profileImage {
+      "image":image.asset, 
+      title, 
+      alt, 
+      caption
+    }, 
+    bio, 
+    bioTitle
+   }
+   [0]`)
+
+  const mdxSource = await serialize(about.bio, {
+    mdxOptions: {
+      rehypePlugins: [rehypePrism],
+      remarkPlugins: [remarkGfm],
+    },
+  })
+
+  https: return {
+    props: {
+      about: about,
+      bio: mdxSource,
+    },
+  }
 }
