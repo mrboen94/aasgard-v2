4f2c190cb840b9454d12c30fed7c2fd29945b47b
Mathias Bøe <mrboen94@gmail.com>
2023-02-24 15:14:33 +0100

    shhh..

--- src/pages/photography.tsx
+++ src/pages/photography.tsx
@@ -7,28 +7,46 @@ import Thumbnails from 'yet-another-react-lightbox/plugins/thumbnails'
 import Zoom from 'yet-another-react-lightbox/plugins/zoom'
 import Lightbox from 'yet-another-react-lightbox'
 import PhotoAlbum from 'react-photo-album'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import 'yet-another-react-lightbox/styles.css'
 import 'yet-another-react-lightbox/plugins/thumbnails.css'
+import Head from 'next/head'
 
 export default function Photography({ images, title, description }) {
+  const [headTitle, setHeadTitle] = useState('Photography 📷 - Mathias Bøe')
+
+  useEffect(() => {
+    window.addEventListener('mousedown', (event) => {
+      setHeadTitle('Photography 📸 - Mathias Bøe')
+    })
+    window.addEventListener('mouseup', (event) => {
+      setHeadTitle('Photography 📷 - Mathias Bøe')
+    })
+  }, [])
+
   const [index, setIndex] = useState(-1)
   return (
-    <SimpleLayout title={title} intro={description}>
-      <PhotoAlbum
-        layout="rows"
-        photos={images}
-        onClick={({ index }) => setIndex(index)}
-      />
-      <Lightbox
-        slides={images}
-        open={index >= 0}
-        index={index}
-        close={() => setIndex(-1)}
-        // enable optional lightbox plugins
-        plugins={[Fullscreen, Slideshow, Thumbnails, Zoom]}
-      />
-    </SimpleLayout>
+    <>
+      <Head>
+        <title>{headTitle}</title>
+        <meta name="description" content="Photography - Mathias Bøe" />
+      </Head>
+      <SimpleLayout title={title} intro={description}>
+        <PhotoAlbum
+          layout="rows"
+          photos={images}
+          onClick={({ index }) => setIndex(index)}
+        />
+        <Lightbox
+          slides={images}
+          open={index >= 0}
+          index={index}
+          close={() => setIndex(-1)}
+          // enable optional lightbox plugins
+          plugins={[Fullscreen, Slideshow, Thumbnails, Zoom]}
+        />
+      </SimpleLayout>
+    </>
   )
 }
 
