21b28fa75051ed985a984e9b34c07ea666ac5422
mrboen94 <mrboen94@gmail.com>
2025-03-08 14:18:36 +0100

    start of new simpler blog/website

---
+++ frontend/src/lib/variables.ts
@@ -0,0 +1,42 @@
+// Set any item to undefined to remove it from the site or to use the default value
+
+export const GLOBAL = {
+  // Site metadata
+  username: "mrboen94",
+  rootUrl: "https://forcepushmain.dev/",
+  shortDescription: "Developer and Nerd",
+  longDescription: "Helping making the world a better place with Ulriken Consulting",
+  
+  // Social media links
+  githubProfile: "https://github.com/mrboen94",
+  bskyProfile: "https://bsky.app/profile/forcepushmain.dev",
+  
+  // Common text names used throughout the site
+  articlesName: "Blogposts",
+  projectsName: "Projects",
+  viewAll: "View All",
+  
+  // Common descriptions used throughout the site
+  noArticles: "No featured Blogposts.",
+  noProjects: "No featured projects yet.",
+
+  // Blog metadata
+  blogTitle: "Random Scribbles & Thoughts",
+  blogShortDescription: "Practical wisdom, unfiltered thoughts, and hot takes.",
+  blogLongDescription: "Development, Design, Photography and some Norwegian local politics",
+
+  // Project metadata
+  projectTitle: "Projects and Code",
+  projectShortDescription: "A list of projects and developer tools.",
+  projectLongDescription: "All of my projects.",
+
+  // Profile image
+  profileImage: "./profile.webp",
+
+  // Menu items
+  menu: {
+    home: "/",
+    projects: "/projects",
+    blog: "/blog",
+  }
+};
